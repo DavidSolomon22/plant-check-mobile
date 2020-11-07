881c82017686ff9e9fa0a9c5d0c0fc36a269583c
David Solomon <dav.soli22@gmail.com>
2020-11-07 17:44:55 +0100

    HomeScreen

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SinglePlant from './components/SinglePlant';
+import Home from './screens/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>PLANT IDENTIFCATION</Text>
+      <Home />
+      {/* <Text>PLANT IDENTIFCATION</Text>
       <Text>HISTORY</Text>
       <StatusBar style="auto" />
-      <SinglePlant plantName="CACTUS" />
+      <SinglePlant plantName="CACTUS" /> */}
       {/* <SinglePlant plantName="CACTUS1" />
       <SinglePlant plantName="CACTUS2" /> */}
     </View>
