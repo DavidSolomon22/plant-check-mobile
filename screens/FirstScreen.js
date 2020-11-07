b3d1ce67804a16e91e620b57875fb955bd319e3e
Jacek Eichler <39658861+ycek21@users.noreply.github.com>
2020-11-07 13:19:22 +0100

    Tab navigation with stack navigations
    Plain navigation without icons and formatting

---
+++ screens/FirstScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const FirstScreen = () => {
+  return (
+    <View>
+      <Text>FirstScreen</Text>
+    </View>
+  );
+};
+
+export default FirstScreen;
