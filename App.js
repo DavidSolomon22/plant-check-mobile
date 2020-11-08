ccdf2f396136b9a0875523a3e641490e52e62b40
David Solomon <dav.soli22@gmail.com>
2020-11-08 17:30:59 +0100

    test crlf

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
