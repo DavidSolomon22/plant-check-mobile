79294d8933abd7e557a73f395f298bade377dccc
Jacek Eichler <39658861+ycek21@users.noreply.github.com>
2020-11-22 15:18:47 +0100

    Photo can be taken and displayed after
    Navigate to single Plant screen

--- screens/TakePhotoScreen.js
+++ screens/TakePhotoScreen.js
@@ -1,38 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+} from 'react-native';
 import { Camera } from 'expo-camera';
-import React, { useEffect, useState } from 'react';
-import * as tf from '@tensorflow/tfjs';
-import { cameraWithTensors } from '@tensorflow/tfjs-react-native';
-import { Platform, StyleSheet } from 'react-native';
-import * as mobilenet from '@tensorflow-models/mobilenet';
+import stylesGlobal from '../styles/style';
 
-import { View, Text } from 'react-native';
-import { setdiff1dAsync } from '@tensorflow/tfjs';
-import { useWindowDimensions } from 'react-native';
-
-const TensorCamera = cameraWithTensors(Camera);
-
-export default function App(props) {
-  const [tfReady, setTfReady] = useState(false);
-  const [model, setModel] = useState(false);
-  const [displayText, setDisplayText] = useState('loading models');
-  const windowWidth = useWindowDimensions().width;
-  const windowHeight = useWindowDimensions().height;
-  useEffect(() => {
-    let checkTf = async () => {
-      console.log('loading models');
-      await tf.ready();
-      console.log('tf ready loading, mobileNet');
-      const model = await mobilenet.load();
-      console.log('modelnet loaded');
-      setModel(model);
-      setDisplayText('loaded Models');
-      setTfReady(true);
-    };
-    checkTf();
-  }, []);
-
+const TakePhotoScreen = () => {
   const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const [photo, setPhoto] = useState(null);
+
+  const handleTakePhoto = () => {
+    async () => {
+      if (cameraRef) {
+        let photoFromCamera = await cameraRef.takePictureAsync();
+        setPhoto(photoFromCamera);
+      }
+    };
+  };
 
   useEffect(() => {
     (async () => {
@@ -44,88 +34,86 @@ export default function App(props) {
   if (hasPermission === null) {
     return <View />;
   }
+
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
-  let AUTORENDER = true;
-  async function handleCameraStream(images, updatePreview, gl) {
-    const loop = async () => {
-      if (!AUTORENDER) {
-        updatePreview();
-      }
-      const imageTensor = images.next().value;
-      const prediction = await model.classify(imageTensor);
-      const highestPropPred = prediction[0];
-      const surity = Math.round(highestPropPred.probability * 100);
-      if (surity > 30) {
-        setDisplayText(highestPropPred.className + ' \n' + surity + '% sure');
-      } else {
-        setDisplayText('not sure what that is');
-      }
-      tf.dispose([imageTensor]);
 
-      if (!AUTORENDER) {
-        gl.endFrameEXP();
-      }
-      requestAnimationFrame(loop);
-    };
-
-    loop();
-  }
-
-  // Currently expo does not support automatically determining the
-  // resolution of the camera texture used. So it must be determined
-  // empirically for the supported devices and preview size.
-
-  let textureDims;
-  if (Platform.OS === 'ios') {
-    textureDims = {
-      height: 1920,
-      width: 1080,
-    };
-  } else {
-    textureDims = {
-      height: 1200,
-      width: 1600,
-    };
+  if (photo) {
+    return <ImageBackground source={photo} style={styles.container} />;
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={{ height: windowHeight * 0.1 }}>{displayText}</Text>
-
-      {tfReady ? (
-        <TensorCamera
-          // Standard Camera props
-          style={{
-            zIndex: -10,
-            width: windowWidth * 0.7,
-            height: windowHeight * 0.7,
+    <Camera
+      style={styles.container}
+      type={type}
+      ref={(ref) => {
+        setCameraRef(ref);
+      }}
+    >
+      <View style={styles.takePhotoBox}>
+        <TouchableOpacity onPress={handleTakePhoto}>
+          <View style={styles.takePhotoButtonRoundShapeBox}>
+            <View style={styles.takePhotoButton} />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.flipButton}
+          onPress={() => {
+            setType(
+              type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back,
+            );
           }}
-          type={Camera.Constants.Type.back}
-          // Tensor related props
-          cameraTextureHeight={textureDims.height}
-          cameraTextureWidth={textureDims.width}
-          resizeHeight={200}
-          resizeWidth={152}
-          resizeDepth={3}
-          onReady={handleCameraStream}
-          autorender={AUTORENDER}
-        />
-      ) : (
-        <View />
-      )}
-    </View>
+        >
+          <Text style={[styles.flipText, stylesGlobal.font]}> Flip </Text>
+        </TouchableOpacity>
+      </View>
+    </Camera>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
-    backgroundColor: '#eaeaea',
+    justifyContent: 'flex-end',
     flexDirection: 'column',
+  },
+  flipText: {
+    fontSize: 25,
+    marginBottom: 10,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  takePhotoBox: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginBottom: '8%',
+  },
+  takePhotoButtonRoundShapeBox: {
+    borderWidth: 2,
+    borderRadius: 50,
+    borderColor: 'white',
+    height: 60,
+    width: 60,
+    display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
   },
+  takePhotoButton: {
+    borderWidth: 2,
+    borderRadius: 50,
+    borderColor: 'white',
+    height: 50,
+    width: 50,
+    backgroundColor: 'white',
+  },
+  flipButton: {
+    position: 'absolute',
+    right: '3%',
+  },
 });
+
+export default TakePhotoScreen;
